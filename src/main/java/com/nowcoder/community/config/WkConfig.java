721bc9851d516facfd64f8204ba8b706fa293aa4
cxmshine <13724591964@163.com>
2019-11-13 11:21:59 +0800

    wkhtmltoimage生成长图以分享

---
+++ src/main/java/com/nowcoder/community/config/WkConfig.java
@@ -0,0 +1,30 @@
+package com.nowcoder.community.config;
+
+import java.io.File;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xuming
+ * @Date 2019/11/13 10:57
+ */
+@Configuration
+public class WkConfig {
+    private static final Logger logger = LoggerFactory.getLogger(WkConfig.class);
+
+    @Value("${wk.image.storage}")
+    private String wkImageStorage;
+
+    @PostConstruct
+    public void init() {
+        // 创建WK图片目录
+        File file = new File(wkImageStorage);
+        if (!file.exists()) {
+            file.mkdir();
+            logger.info("创建WK图片目录: " + wkImageStorage);
+        }
+    }
+}
