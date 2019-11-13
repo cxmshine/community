28147592292faad85570077998922a5df702212c
cxmshine <13724591964@163.com>
2019-11-13 23:29:07 +0800

    利用本地缓存caffeine做性能优化

---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author xuming
+ * @Date 2019/11/13 22:27
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+    @Autowired
+    private DiscussPostService postService;
+
+    @Test
+    public void testCache() {
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 0));
+    }
+}
